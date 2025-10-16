efd3c1ce5391841d24f485bef8a0514f206036b2
talharasheedclan-gif <talharasheedclan@gmail.com>
2025-10-16 16:49:42 +0400

    Update main.py

--- main.py
+++ main.py
@@ -60,4 +60,4 @@ async def startup():
     asyncio.create_task(scheduler())
 
 if __name__ == "__main__":
-    uvicorn.run("src.main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8080)), reload=False)
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT", 8080)), reload=False)
