97a3b350dca47de7d97a54b9f4178442ee5e11d3
talharasheedclan-gif <talharasheedclan@gmail.com>
2025-10-16 08:39:58 +0400

    Update exchange_ws.py

--- exchange_ws.py
+++ exchange_ws.py
@@ -1,7 +1,7 @@
 import asyncio, pandas as pd
 from datetime import datetime, timezone
 from binance import AsyncClient, BinanceSocketManager
-from .indicators import rsi, ema, vwap, vwap_deviation_bands, vwap, liquidity_sweep
+from indicators import rsi, ema, vwap, vwap_deviation_bands, vwap, liquidity_sweep
 from .config import settings
 from .alert_router import AlertRouter
 
